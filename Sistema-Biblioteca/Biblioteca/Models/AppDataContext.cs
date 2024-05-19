21b1404d3272c2f8ab36d14ca278542df477d839
joao-moorgado <joao.moorgado@gmail.com>
2024-05-18 21:47:15 -0300

    Adicao tabelas db

--- Sistema-Biblioteca/Biblioteca/Models/AppDataContext.cs
+++ Sistema-Biblioteca/Biblioteca/Models/AppDataContext.cs
@@ -4,7 +4,10 @@ namespace Biblioteca.Models;
 
 public class AppDataContext : DbContext {
 
+    public DbSet<Livro> Livros { get; set; }
     public DbSet<Autor> Autores { get; set; }
+    public DbSet<Genero> Generos { get; set; }
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
